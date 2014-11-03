8d5af671eeea0346498cfc5af18e9472b6b13637
Marcelo Zapaia <mzapaia@Admins-MacBook-Pro.local>
2014-11-03 15:10:14 -0300

    Commit message

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-exports.middleware = require('./middleware');
+// exports.middleware = require('./middleware');
 exports.load = require('./load');
-exports.gulp = require('./gulp');
+
+exports.gulp = function(gulp,conf){
+	var zetamGulpConf = require('./gulp');
+	zetamGulpConf.apply(zetamGulpConf,arguments);
+};
+
+exports.middleware = function(){
+	var zetamMiddleware = require('./middleware');
+	zetamMiddleware.apply(zetamMiddleware,arguments);
+};
